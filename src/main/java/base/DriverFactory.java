405e5a0bfdef34fd5d51d560587b3ceb99dd1fbe
muthamgarivenu363@gmail.com <muthamgarivenu234@gmail.com>
2025-11-28 15:37:19 +0700

    hgdjhs

--- src/main/java/base/DriverFactory.java
+++ src/main/java/base/DriverFactory.java
@@ -4,28 +4,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import utils.ConfigReader;
 
 public class DriverFactory {
 
     private static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
 
     public static WebDriver initDriver(String browserName) {
+
         System.out.println("🧠 Launching browser: " + browserName);
 
+        // Read headless setting from config.properties
+        String headlessValue = ConfigReader.initProperties().getProperty("headless", "false");
+        boolean isHeadless = headlessValue.equalsIgnoreCase("true");
+
         if (browserName.equalsIgnoreCase("chrome")) {
+
             WebDriverManager.chromedriver().setup();
 
             ChromeOptions options = new ChromeOptions();
-
-            // --------- ⭐ IMPORTANT FOR JENKINS ⭐ ----------
-            // Chrome will NOT open in Jenkins unless it's headless
-            options.addArguments("--headless=new");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--window-size=1920,1080");
             options.addArguments("--remote-allow-origins=*");
-            // -------------------------------------------------
+
+            if (isHeadless) {
+                // Jenkins-friendly headless mode
+                System.out.println("🚀 Running in HEADLESS mode (Jenkins)");
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--disable-gpu");
+                options.addArguments("--window-size=1920,1080");
+            } else {
+                // Local mode
+                System.out.println("🖥️ Running in NORMAL mode (Local)");
+            }
 
             tlDriver.set(new ChromeDriver(options));
         }
@@ -41,6 +52,7 @@ public class DriverFactory {
         if (getDriver() != null) {
             getDriver().quit();
             tlDriver.remove();
+            System.out.println("🧹 Driver closed and removed from ThreadLocal");
         }
     }
 }
